dfcbc43744841cf4c193a733efbc2f23a71e6a1a
程序员阿江(Relakkes) <relakkes@gmail.com>
2026-09-09 21:42:10 +0800

    release: v0.1.3

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,6 +1,6 @@
 export * from "./errors.js";
 export * from "./schemas.js";
-export const RUNTIME_VERSION = "0.1.2";
+export const RUNTIME_VERSION = "0.1.3";
 export const DEFAULT_PORT = 47831;
 export const API_PREFIX = "/v1";
 export * from "./attachments.js";
